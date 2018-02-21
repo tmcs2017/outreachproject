983ffee808967ac6dcf883784c1d247f0603acc8
Alex Jamieson-Binnie <binnie567@gmail.com>
2018-02-21 17:21:45 +0000

    Added name and description variables to MoleculeDefinition

--- Assets/Scripts/MoleculeDefinition.cs
+++ Assets/Scripts/MoleculeDefinition.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Molecule", menuName = "Molecule")]
 public class MoleculeDefinition : ScriptableObject {
 
+	public string Name;
+	public string Description;
+
 	public List<AtomDefinition> Atoms = new List<AtomDefinition>();
 
 	public List<VibrationalModeDefinition>  VibrationalModes = new List<VibrationalModeDefinition>();
