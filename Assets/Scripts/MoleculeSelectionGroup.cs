5cae71f7d60330951c438665b130b5e3b27ed45f
Alex Jamieson-Binnie <binnie567@gmail.com>
2018-03-19 23:38:35 +0000

    ScriptableObject to store the possible selection screens (space, atmosphere)

---
+++ Assets/Scripts/MoleculeSelectionGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An object which represents a set of molecules to be displayed for selection, i.e. the Atmosphere or Space view
+/// </summary>
+[CreateAssetMenu(fileName = "Selection Group", menuName = "Selection Group")]
+public class MoleculeSelectionGroup : ScriptableObject {
+
+    public List<MoleculeDefinition> Definitions = new List<MoleculeDefinition>();
+
+}
